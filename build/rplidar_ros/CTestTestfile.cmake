4e55050d1a0048818ae5b0f9f4f6287ded946d29
BertHat <b.van.hattem@xmsnet.nl>
2025-09-14 21:22:34 +0200

    Remove build, install, log directories from repo

--- build/rplidar_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/berth/rmb/src/rmb_db/rplidar_ros
-# Build directory: /home/berth/rmb/src/rmb_db/build/rplidar_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
